7c42db9b64f9e212e383579fbea16290c0afcdac
WAvsAC <mohajonjoy6@gmail.com>
2022-04-22 02:12:18 +0600

    update updateProfile feature

--- src/components/UpdateProfile.js
+++ src/components/UpdateProfile.js
@@ -1,5 +1,88 @@
-import React from "react";
+import React, { useState, useRef } from "react";
+import { Card, Form, Button, Alert } from "react-bootstrap";
+import { Link, useNavigate } from "react-router-dom";
+import { useAuth } from "../Contexts/AuthContext";
 
 export default function UpdateProfile() {
-  return <div>UpdateProfile</div>;
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const confirmPasswordRef = useRef();
+  const { currentUser, updateEmail, updatePassword } = useAuth();
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  function submitHandler(e) {
+    e.preventDefault();
+
+    if (passwordRef.current.value !== confirmPasswordRef.current.value) {
+      return setError("Password doesn't match!");
+    }
+
+    const promise = [];
+    if (currentUser.email !== emailRef.current.value) {
+      promise.push(updateEmail(emailRef.current.value));
+    }
+    if (currentUser.password !== passwordRef.current.value) {
+      promise.push(updatePassword(passwordRef.current.value));
+    }
+
+    Promise.all(promise)
+      .then(() => {
+        navigate("/");
+      })
+      .catch(() => {
+        setError("Failed to update profile.");
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }
+
+  // console.log(emailRef);
+  return (
+    <>
+      <Card>
+        <Card.Body>
+          <h2 className="text-center mb-4">Update Profile</h2>
+          {error && <Alert variant="danger">{error}</Alert>}
+          <Form onSubmit={submitHandler}>
+            <Form.Group id="email" className="mb-3">
+              <Form.Label>Email</Form.Label>
+              <Form.Control
+                type="email"
+                ref={emailRef}
+                required
+                defaultValue={currentUser.email}
+              />
+            </Form.Group>
+            <Form.Group id="password" className="mb-3">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                ref={passwordRef}
+                required
+                placeholder="Leave blank to keep same"
+              />
+            </Form.Group>
+            <Form.Group id="confirm-password" className="mb-3">
+              <Form.Label>Confirm Password</Form.Label>
+              <Form.Control
+                type="password"
+                ref={confirmPasswordRef}
+                required
+                placeholder="Leave blank to keep same"
+              />
+            </Form.Group>
+            <Button disabled={loading} className="w-100" type="submit">
+              Update
+            </Button>
+          </Form>
+        </Card.Body>
+      </Card>
+      <div className="w-100 text-center mt-2">
+        <Link to="/">Cancel</Link>
+      </div>
+    </>
+  );
 }
